16abc12d1cf840438383ec25a66d423123dd6453
AdhithyanDas <adhithyandask@gmail.com>
2024-12-28 17:04:13 +0530

    completed

--- Socket/socket.js
+++ Socket/socket.js
@@ -7,7 +7,7 @@ const chatApp = express()
 const server = http.createServer(chatApp)
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:5173"],
+        origin: ["https://chat-application-plum-rho.vercel.app"],
         methods: ["POST", "GET", "PUT", "DELETE"]
     }
 })
